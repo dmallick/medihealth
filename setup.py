087bcc6eb6977d2f4caa44c9d966565db6441fb7
Debasish Mallick <debasish.mallick@gmail.com>
2025-07-03 20:55:19 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GenAI MediHealth',
+    version= '0.0.1',
+    author= 'Debasish Mallick',
+    author_email= 'debasish.mallick@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
